47e50ccf001e49eb3cffc236a401020c107251fa
Chris Allen <callen7908@yahoo.com>
2021-04-30 14:51:19 -0400

    Working on board

--- src/index.js
+++ src/index.js
@@ -6,17 +6,39 @@ import './index.css';
 // Then enter "npm start"
 
 
+// In JavaScript classes, you need to always call 'super' when defining the constructor of a subclass
+// All React component classes that have a constructor should start with a super(props) call
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }
+
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         );
     }
 }
 
+/*
+To collect data from multiple children, or to have two child components communicate with each other,
+you need to declare the shared state in their parent component instead.
+The parent component can pass the state back down to the children by using props;
+this keeps the child components in sync with each other and with the parent component.
+ */
 class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
+
     renderSquare(i) {
         return <Square value={i} />;
     }
